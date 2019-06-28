96a80657a9e0ce021cac1c2cacb660fe0de1eb30
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-06-28 06:55:42 -0700

    configured est environments

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,10 @@
 module.exports = {
   development: {
       client: 'sqlite3',
-      useNullAsDefault: true,
       connection: {
           filename: './data/movies.db3',
       },
+      useNullAsDefault: true,
       pool: {
           afterCreate: (conn, done) => {
               conn.run('PRAGMA foreign_keys = ON', done);
@@ -16,11 +16,23 @@ module.exports = {
       seeds: {
           directory: './data/seeds',
       },
-      testing: {
-        client: 'sqlite3',
-        connection: {
-          filename: './data/test.db3'
-        }
-      }
   },
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/test.db3'
+    },
+    useNullAsDefault: true,
+      pool: {
+          afterCreate: (conn, done) => {
+              conn.run('PRAGMA foreign_keys = ON', done);
+          },
+      },
+      migrations: {
+          directory: './data/migrations',
+      },
+      seeds: {
+          directory: './data/seeds',
+      }
+  }
 };
